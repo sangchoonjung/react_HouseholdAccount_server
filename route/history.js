a96a6ad46a975b1e81dd267b877747fd2c5091cb
sangchoonjung <109258408+sangchoonjung@users.noreply.github.com>
2022-12-29 13:07:22 +0900

    221229

--- route/history.js
+++ route/history.js
@@ -1,4 +1,4 @@
-import express, { json } from "express";
+import express from "express";
 import jwt from "jsonwebtoken";
 import history from "../model/history.js";
 
@@ -7,13 +7,14 @@ const router = express.Router();
 // auth token check middleware
 router.use((req, res, next) => {
   const authorization = req.get("authorization");
+  // console.log(authorization.startsWith("Bearer"), "헤더")
   if (!authorization || !authorization.startsWith("Bearer")) {
     return res
       .status(401)
       .json({ result: false, message: "토큰이 존재하지않습니다." });
   }
   const token = authorization.split(" ")[1];
-  // console.log(token)
+
   try {
     const payload = jwt.verify(token, process.env.SECRET_KEY);
     //정상작동이면 payload안에 email이 들어있다.
@@ -24,6 +25,7 @@ router.use((req, res, next) => {
   next();
 });
 
+//내용 넣기
 router.post("/write", async (req, res) => {
   const account = req.logonEmail;
   console.log(account);
@@ -36,15 +38,21 @@ router.post("/write", async (req, res) => {
   }
 });
 
+//조회
 router.get("/", async (req, res) => {
   const account = req.logonEmail;
   const month = req.query.month;
   console.log(month);
 
   let datas = await history.find({ account: account }).lean();
+  datas = datas.filter((one) => {
+    return one.itemDate.startsWith(month);
+  });
   console.log(datas);
   return res.status(200).json({ result: true, datas: datas });
 });
+
+//검색
 
 router.get("/search", async (req, res) => {
   const begin = new Date(req.query.begin).toISOString().slice(0, 10);
@@ -62,16 +70,25 @@ router.get("/search", async (req, res) => {
       })
       .sort("itemDate")
       .lean();
-    console.log(datas.length);
+
     return res.status(200).json({ result: true, datas: datas });
   } catch (err) {
     res.status(401).json({ result: false, message: err.message });
   }
 });
 
-router.get("/delete", (req, res) => {
-  console.log(req.logonEmail);
-  return res.status(200).json({ result: false });
+//삭제
+
+router.post("/delete", async (req, res) => {
+  const { data } = req.body;
+  console.log(data, "아이디");
+  try {
+    const response = await history.deleteMany({ _id: { $in: data } });
+    console.log(response)
+    return res.status(200).json({ result: true, message: response });
+  } catch (e) {
+    return res.status(401).json({ result: false, message: "삭제안됨" })
+  }
 });
 
 export default router;
